a1f183aeaaa77d74728d7a6ca82dbc47113b72ec
Agisight <31446954+Agisight@users.noreply.github.com>
2025-10-22 04:06:48 -0700

    Update Package.swift

--- packages/stripe_ios/ios/stripe_ios/Package.swift
+++ packages/stripe_ios/ios/stripe_ios/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // .package(url: "https://github.com/stripe/stripe-ios-spm", exact: "24.23.0")
-        .package(url: "https://github.com/Agisight/stripe-ios-spm", exact: "24.23.0")
+        .package(url: "https://github.com/Agisight/stripe-ios-spm", exact: "lim")
     ],
     targets: [
         .target(
